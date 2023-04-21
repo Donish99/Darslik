73a5cd9e7867df09acafe4bb77799f9f3cca27f6
Donish <nurakh99@gmail.com>
2023-04-21 10:54:01 +0500

    catch will not work with webhooks

--- api/bot.ts
+++ api/bot.ts
@@ -30,19 +30,19 @@ bot.callbackQuery(
     })
 );
 
-// Start the bot.
-bot.start();
-bot.catch((err) => {
-  const ctx = err.ctx;
-  console.error(`Error while handling update ${ctx.update.update_id}:`);
-  const e = err.error;
-  if (e instanceof GrammyError) {
-    console.error("Error in request:", e.description);
-  } else if (e instanceof HttpError) {
-    console.error("Could not contact Telegram:", e);
-  } else {
-    console.error("Unknown error:", e);
-  }
-});
-
 export default webhookCallback(bot, "https");
+
+// Start the bot.
+// bot.catch((err) => {
+//   const ctx = err.ctx;
+//   console.error(`Error while handling update ${ctx.update.update_id}:`);
+//   const e = err.error;
+//   if (e instanceof GrammyError) {
+//     console.error("Error in request:", e.description);
+//   } else if (e instanceof HttpError) {
+//     console.error("Could not contact Telegram:", e);
+//   } else {
+//     console.error("Unknown error:", e);
+//   }
+// });
+// bot.start();
