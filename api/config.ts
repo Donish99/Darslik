83470e7ceffee47561d3fbf61daa2433eea60547
Donish <nurakh99@gmail.com>
2023-04-21 17:56:05 +0500

    env stuff

--- api/config.ts
+++ api/config.ts
@@ -1,11 +1,6 @@
 import * as dotenv from "dotenv";
 
 const envFound = dotenv.config();
-if (envFound.error) {
-  // This error should crash whole process
-
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
 
 const CONFIG = {
   admin_chat_id: Number(process.env.ADMIN_CHAT_ID)!,
@@ -20,5 +15,7 @@ const CONFIG = {
     connection_string: process.env.CON_STRING!,
   },
 };
+
+console.log(CONFIG);
 
 export default CONFIG;
